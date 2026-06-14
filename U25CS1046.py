620a5108b5ddc52b1637c4e4c7a2ad126f532931
alaminmad67-debug <alaminmad67@gmail.com>
2026-06-14 14:13:24 +0100

    Update U25CS1046.py

--- U25CS1046.py
+++ U25CS1046.py
@@ -1,14 +1,12 @@
-import math
-
 a = float(input("Enter a: "))
 b = float(input("Enter b: "))
 c = float(input("Enter c: "))
 
-discriminant = b**2 - 4*a*c
+inroot = b**2 - 4*a*c
 
-if discriminant >= 0:
-    root1 = (-b + math.sqrt(discriminant)) / (2*a)
-    root2 = (-b - math.sqrt(discriminant)) / (2*a)
+if inroot >= 0:
+    root1 = (-b + inroot ** 0.5) / (2*a)
+    root2 = (-b - inroot ** 0.5) / (2*a)
     print("Root 1:", root1)
     print("Root 2:", root2)
 else:
