343f258c1edc33faeedaaf6eba347686b29c6d2d
alaminmad67-debug <alaminmad67@gmail.com>
2026-06-14 14:08:46 +0100

    Committee Muhammad

---
+++ U25CS1046.py
@@ -0,0 +1,15 @@
+import math
+
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("Enter c: "))
+
+discriminant = b**2 - 4*a*c
+
+if discriminant >= 0:
+    root1 = (-b + math.sqrt(discriminant)) / (2*a)
+    root2 = (-b - math.sqrt(discriminant)) / (2*a)
+    print("Root 1:", root1)
+    print("Root 2:", root2)
+else:
+    print("No real roots")
